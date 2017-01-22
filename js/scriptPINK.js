bc3f76c7bb91a03e2324ff3ab7acfdd8bcb22b92
shanegibney <shanegibney@gmail.com>
2017-01-22 20:46:54 +0000

    homepage gif behaviour set

--- js/scriptPINK.js
+++ js/scriptPINK.js
@@ -105,8 +105,8 @@ function displayImage() {
   var num = Math.floor(Math.random() * 7); // 0...6
   //the second statement display the random image from the imagesArray array in the canvas image using the random number as the subscript value
   // document.canvas.src = imagesArray[num];
-  document.canvas.src = "../images/No_Pink/HP_gifstop_" + (num + 1) + ".png";
-  // document.canvas.src = "../images/Pink/HP_gifstop" + (num + 1) + "_pink.png";
+  // document.canvas.src = "../images/No_Pink/HP_gifstop_" + (num + 1) + ".png";
+  document.canvas.src = "../images/Pink/HP_gifstop" + (num + 1) + "_pink.png";
   // HP_gifstop7_pink.png
 
 }
