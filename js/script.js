8ff415e42b4e8d3d5decc6eb1e581114e2a16225
shanegibney <shanegibney@gmail.com>
2017-02-02 23:02:34 +0000

    correct main gif behaviour for lg device

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var logoBodyWidth = $(".logoBody").width();
 var mainGifWidth = $(".mainGif").width();
 var mainGifWidthMobile = $(".mainGif-mobile").width();
 var windowWidth = window.innerWidth;
-var gifMobPos = (windowWidth/2) - (mainGifWidthMobile/2);
+var gifMobPos = (windowWidth / 2) - (mainGifWidthMobile / 2);
 
 console.log('logoBodyWidth = ' + logoBodyWidth);
 console.log('mainGifWidthMobile = ' + mainGifWidthMobile);
@@ -44,13 +44,13 @@ function resetColour() {
     $(".text" + rand3).css("color", "white")
 };
 
-function arrive(){
-    console.log("mouse is on")
-}
-
-function leave(){
-    console.log("mouse is out")
-}
+// function arrive(){
+//     console.log("mouse is on")
+// }
+//
+// function leave(){
+//     console.log("mouse is out")
+// }
 
 function repeat() {
     // interval = window.setInterval(function() { console.log("Mouse is on image") }, 1000)
@@ -60,15 +60,15 @@ function repeat() {
 };
 $(".mainGif").on({
     mouseover: function() {
-      arrive(),
+        // arrive(),
         repeat(),
-        displayImage();
+            displayImage();
     },
     mouseout: function() {
         window.clearInterval(interval),
-        leave(),
-        resetColour(),
-        resetGif();
+            // leave(),
+            resetColour(),
+            resetGif();
     }
 });
 
@@ -96,16 +96,22 @@ function randomHighlighting() {
 // var imagesArray = ["images/head2.gpg", "images/head3.jpg"];
 
 function displayImage() {
+    console.log("this is the displayImage() function");
     //the first statement should generate a random number in the range 0 to 6 (the subscript values of the image file names in the imagesArray)
     var num = Math.floor(Math.random() * 7); // 0...6
     //the second statement display the random image from the imagesArray array in the canvas image using the random number as the subscript value
     // document.canvas.src = imagesArray[num];
-    document.canvas.src = "images/No_Pink/HP_gifstop_" + (num + 1) + ".png";
+    console.log(num);
+    // document.canvas.src = "images/No_Pink/HP_gifstop_" + (num + 1) + ".png";
+    // document.canvas.src = "images/head1.jpg";
+    $(".mainGif").attr("src", "images/No_Pink/HP_gifstop_" + (num + 1) + ".png");
     // document.canvas.src = "Pink/HP_gifstop" + (num + 1) + "_pink.png";
     // HP_gifstop7_pink.png
-
+    // console.log(document.canvas.src);
 }
 
 function resetGif() {
-    document.canvas.src = "images/HPGIF_slow.gif";
+    // document.canvas.src = "images/HPGIF_slow.gif";
+    // console.log("This is the resetGif() function");
+    $(".mainGif").attr("src", "images/HPGIF_slow.gif");
 }
