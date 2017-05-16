b98a826b613d5f311a08816136214daf71c07ff7
shanegibney <shanegibney@gmail.com>
2017-05-16 21:09:31 +0100

    updates on tues 16 May 2017

--- js/script.js
+++ js/script.js
@@ -27,21 +27,21 @@ $(".mainGif").css("left", ((logoBodyWidth - mainGifWidth) / 2) + "px");
 $(".mainGif-mobile").css("left", ((logoBodyWidth - mainGifWidthMobile) / 2) + "px");
 
 $(".button").click(function() {
-    $(".phase").toggle();
+  $(".phase").toggle();
 });
 
 var rand1, rand2, rand3;
 var interval;
 
 function randomIt() {
-    return Math.floor(Math.random() * 102)
+  return Math.floor(Math.random() * 102)
 };
 
 function resetColour() {
-    $(".text0").css("color", "white")
-    $(".text" + rand1).css("color", "white")
-    $(".text" + rand2).css("color", "white")
-    $(".text" + rand3).css("color", "white")
+  $(".text0").css("color", "white")
+  $(".text" + rand1).css("color", "white")
+  $(".text" + rand2).css("color", "white")
+  $(".text" + rand3).css("color", "white")
 };
 
 // function arrive(){
@@ -53,57 +53,58 @@ function resetColour() {
 // }
 
 function repeat() {
-    // interval = window.setInterval(function() { console.log("Mouse is on image") }, 1000)
-    // interval = window.setInterval(randomHighlighting, 3000)
-    console.log("repeat() function is working");
-    randomHighlighting();
+  // interval = window.setInterval(function() { console.log("Mouse is on image") }, 1000)
+  // interval = window.setInterval(randomHighlighting, 3000)
+  console.log("repeat() function is working");
+  randomHighlighting();
 };
 $(".mainGif").on({
-    mouseover: function() {
-        // arrive(),
-        repeat(),
-            displayImage();
-    },
-    mouseout: function() {
-        window.clearInterval(interval),
-            // leave(),
-            resetColour(),
-            resetGif();
-    }
+  mouseover: function() {
+    // arrive(),
+    repeat(),
+      displayImage();
+  },
+  mouseout: function() {
+    window.clearInterval(interval),
+      // leave(),
+      resetColour(),
+      resetGif();
+  }
 });
 
 function randomHighlighting() {
 
-    //prevents first span ever going white.
-    $(".text0").css("color", "#FFA197")
+  //prevents first span ever going white.
+  $(".text0").css("color", "#FFA197")
 
-    if (rand1 != 0) {
-        $(".text" + rand1).css("color", "white")
-    }
-    if (rand2 != 0) {
-        $(".text" + rand2).css("color", "white")
-    }
-    if (rand3 != 0) {
-        $(".text" + rand3).css("color", "white")
-    }
-    rand1 = randomIt();
-    rand2 = randomIt();
-    rand3 = randomIt();
-    $(".text" + rand1).css("color", "#FFA197")
-    $(".text" + rand2).css("color", "#FFA197")
-    $(".text" + rand3).css("color", "#FFA197")
+  if (rand1 != 0) {
+    $(".text" + rand1).css("color", "white")
+  }
+  if (rand2 != 0) {
+    $(".text" + rand2).css("color", "white")
+  }
+  if (rand3 != 0) {
+    $(".text" + rand3).css("color", "white")
+  }
+  rand1 = randomIt();
+  rand2 = randomIt();
+  rand3 = randomIt();
+  $(".text" + rand1).css("color", "#FFA197")
+  $(".text" + rand2).css("color", "#FFA197")
+  $(".text" + rand3).css("color", "#FFA197")
 };
 
 function displayImage() {
-    console.log("this is the displayImage() function");
-    var num = Math.floor(Math.random() * 7); // 0...6
-    console.log(num);
-    $(".mainGif").attr("src", "images/No_Pink/HP_gifstop_" + (num + 1) + ".png");
-    // $(".mainGif").attr("src", "images/Pink/HP_gifstop" + (num + 1) + "_pink.png");
+  console.log("this is the displayImage() function");
+  var num = Math.floor(Math.random() * 7); // 0...6
+  console.log(num);
+  $(".mainGif").attr("src", "images/No_Pink/HP_gifstop_" + (num + 1) + ".png");
+
+  // $(".mainGif").attr("src", "images/Pink/HP_gifstop" + (num + 1) + "_pink.png");
 }
 
 function resetGif() {
-    // document.canvas.src = "images/HPGIF_slow.gif";
-    // console.log("This is the resetGif() function");
-    $(".mainGif").attr("src", "images/HPGIF_slow.gif");
+  // document.canvas.src = "images/HPGIF_slow.gif";
+  // console.log("This is the resetGif() function");
+  $(".mainGif").attr("src", "images/HPGIF_slow.gif");
 }
